0dbd2d9e379e53861b2ac2d56ff421f6cc1ae7a3
Mustafa Karadağ <126167006+karadagmustafa@users.noreply.github.com>
2024-06-14 01:53:20 +0300

    added project pom.xml dependencies

---
+++ src/test/java/management/io/eventManagement/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package management.io.eventManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
